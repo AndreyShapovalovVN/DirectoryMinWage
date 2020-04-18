c3742d199d0dbf3ba4b457f7d81793de11b70b36
AndreyShapovalovVN <a147@ukr.net>
2020-04-18 21:24:40 +0300

    Не значні випровлення

--- settings.py
+++ settings.py
@@ -4,5 +4,6 @@ DEBUG = True
 
 APP_PORT = os.environ.get('LISTEN_PORT') or 8060
 
-TOKEN = 'bb96c2fc40d2d54617d6f276febe571f623a8dadf0b734855299b0e107fda32cf6b69f2da32b36445d73690b93cbd0f7bfc20e0f7f28553d2a4428f23b716e90'
+# token sha512 от userid
 USERID = os.environ.get('userId') or '0123456789'
+TOKEN = 'bb96c2fc40d2d54617d6f276febe571f623a8dadf0b734855299b0e107fda32cf6b69f2da32b36445d73690b93cbd0f7bfc20e0f7f28553d2a4428f23b716e90'
